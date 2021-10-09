137fff170f88bc246fcac6e43b3abf3da2ac0ac4
deepak5998 <deepakkumbhar1920@gmail.com>
2021-10-09 15:24:23 +0530

    added:method for editing the entry

--- src/AddressBook/AddressBook.java
+++ src/AddressBook/AddressBook.java
@@ -1,18 +1,21 @@
 package AddressBook;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 /*
-* Program for address book to create contacts
-* @deepak   started-6/10/21
-*
-*/
-
+ * Program for address book to create contacts
+ * @deepak   started-6/10/21
+ *
+ */
+//UC1-create a contact in address book
 public class AddressBook {
 
-    static Person record = new Person();
-    static ArrayList list = new ArrayList();
-
+    static PersonDetails record = new PersonDetails();
+    static List<PersonDetails> book = new ArrayList<PersonDetails>();
+    static Scanner sc = new Scanner(System.in);
     public static String firstName;                       //Obj Attributes
     public static String lastName;
     public static String address;
@@ -20,45 +23,84 @@ public class AddressBook {
     public static String state;
     public static String zipCode;
     public static String phoneNo;
-    //public static String email;
-
 
 
     public String toString() {
-        return "first name " + firstName + " \nlast name" + lastName+"\naddress"+address+"\ncity"+
-                city+"\nzipcode"+zipCode+"\nphoneno"+phoneNo;
+        return "first name " + firstName + " \nlast name" + lastName + "\naddress" + address + "\ncity" +
+                city + "\nzipcode" + zipCode + "\nphoneNo" + phoneNo;
     }
 
     public static void main(String[] args) {
 
+        while (true) {
+            System.out.println("enter your choice \n1.add entry\n2.edit entry\n3.delete entry");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addEntry();
+                    break;
+                case 2:
+                    edit(book);
+                    break;
+                case 3:
+                    delete(book);
+                    break;
+            }
+        }
+    }
 
-
-        Scanner sc = new Scanner(System.in);
-
-        //asking user input
+    /*asking user input_UC2                Adding entry record to book*/
+    private static void addEntry() {
         System.out.println("Please enter details to be added.");
         System.out.print("First Name: ");
         record.setFirstName(sc.next());
         System.out.print("Last Name: ");
         record.setLastname(sc.next());
         System.out.print("Address: ");
-        //record.setAddress(sc.next());
+        record.setAddress(sc.next());
         System.out.print("City: ");
-        //record.setCity(sc.next());
+        record.setCity(sc.next());
         System.out.print("State: ");
-        //record.setState(sc.next());
+        record.setState(sc.next());
         System.out.print("ZipCode: ");
-        //record.setZipCode(sc.next());
+        record.setZipcode(sc.next());
         System.out.print("Phone No.: ");
         record.setPhoneNo(sc.next());
-        System.out.println("Created entry for " + firstName + " " + lastName);
+        System.out.println("Created entry for " + record.firstName + " " + record.lastName);
 
         record.toString();
-
-        //list.add(record);                    //Adding entry to record
-        //System.out.println(list);
-
-
+        book.add(record);
+        System.out.println(book);
     }
+
+    //method for edit_UC3
+    public static void edit(List<PersonDetails> book) {
+        System.out.println("Please enter name of contact.");
+        String name = sc.next();
+        String lower_name = name.toLowerCase();
+
+        for (PersonDetails i : book) {
+            String nameR = i.firstName.toLowerCase();
+            if (lower_name.equals(nameR)) {
+                System.out.println("Please enter new details.");
+                System.out.print("Address: ");
+                record.address = sc.next();
+                System.out.print("City: ");
+                record.city = sc.next();
+                System.out.print("State: ");
+                record.state = sc.next();
+                System.out.print("ZipCode: ");
+                record.zipCode = sc.next();
+                System.out.print("Phone No.: ");
+                record.phoneNo = sc.next();
+
+                System.out.println("Record updated.");
+                break;
+            }
+
+
+        }
+    }
+
 
 }
---
+++ src/AddressBook/PersonDetails.java
@@ -0,0 +1,77 @@
+package AddressBook;
+
+/*code to get person details*/
+public class PersonDetails {
+
+    public String firstName;
+    public String lastName;
+    public String phoneNo;
+    public String address;
+    public String city;
+    public String zipCode;
+    public String state;
+
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipcode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [firstname=" + firstName + ", lastname=" + lastName + ", address="
+                + address + "]";
+    }
+
+}
